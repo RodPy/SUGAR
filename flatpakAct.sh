34806e0aba1cbe9876b2e3448e86c74717f38e1a
RodPy <rodneyrrs@hotmail.com>
2020-04-04 16:25:11 -0400

    Intalador de las 20 actividades SUGAR

---
+++ flatpakAct.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo Instalando programas de Flathub:
+echo FractionBounce
+flatpak install flathub org.sugarlabs.FractionBounce -y
+flatpak install flathub org.sugarlabs.Dimensions -y
+flatpak install flathub org.sugarlabs.MusicKeyboard -y
+flatpak install flathub org.sugarlabs.ImplodeActivity -y
+flatpak install flathub org.sugarlabs.Sliderule -y
+flatpak install flathub org.sugarlabs.FotoToon -y
+flatpak install flathub org.sugarlabs.AbacusActivity -y
+flatpak install flathub org.sugarlabs.Maze -y
+flatpak install flathub org.sugarlabs.Memorize -y
+flatpak install flathub org.sugarlabs.ReadETexts -y
+flatpak install flathub org.sugarlabs.StoryActivity -y
+flatpak install flathub org.sugarlabs.Measure -y
+flatpak install flathub org.sugarlabs.TurtlePondActivity -y
+flatpak install flathub org.sugarlabs.Words -y
+flatpak install flathub org.sugarlabs.Speak -y
+flatpak install flathub org.sugarlabs.Pippy -y
+flatpak install flathub org.sugarlabs.Physics -y
+flatpak install flathub org.sugarlabs.ColorDeducto -y
+flatpak install flathub org.sugarlabs.Chart -y
+flatpak install flathub org.sugarlabs.Finance -y
